ada3843f368c9fd5b978380df65fa60a86d0fac6
Greg Painter <greg.painter@gmail.com>
2019-06-03 14:31:56 -0600

    Fixing build try 1

--- test/math-test.py
+++ test/math-test.py
@@ -17,4 +17,4 @@ from src.math import Math
 
 class MathTest(unittest.TestCase):
     def test_addition(self):
-        self.assertEqual(Math.addition(3, 4), 8)
+        self.assertEqual(Math.addition(3, 4), 7)

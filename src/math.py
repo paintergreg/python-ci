cafdfd3a69bd27cc62b5919b7cdb4a1dfd8eedf7
Greg Painter <greg.painter@gmail.com>
2019-06-03 15:08:43 -0600

    Added two new tests

--- src/math.py
+++ src/math.py
@@ -14,7 +14,7 @@ docstring goes here.  be sure to write a good one ;)
 
 class Math:
     def addition(value1: int, value2: int) -> int:
-        if not isinstance(value1, int) and not isinstance(value2, int):
+        if not isinstance(value1, int) or not isinstance(value2, int):
             return "Invalid Input"
         else:
             return value1 + value2
--- test/math-test.py
+++ test/math-test.py
@@ -18,3 +18,9 @@ from src.math import Math
 class MathTest(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(Math.addition(3, 4), 7)
+
+    def test_invalid_value1(self):
+        self.assertEqual(Math.addition("A", 3), "Invalid Input")
+
+    def test_invalid_value2(self):
+        self.assertEqual(Math.addition(3, "A"), "Invalid Input")
